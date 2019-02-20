ba84965390aa638d969315091d6aa5d3e30b06d5
Josh Kemmer <josh@joshkemmer.com>
2019-02-20 01:46:04 -0700

    fix grunt uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,57 @@
 // Load Grunt
 module.exports = function (grunt) {
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-  
-      // Tasks
-      sass: { // Begin Sass Plugin
-        dist: {
-          options: {
-            sourcemap: 'none',
-            style: 'compressed'
-          },
-          files: [{
-            'styles/main.min.css': 'styles/scss/main.scss'
-        }]
-        }
-      },
-      postcss: { // Begin Post CSS Plugin
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Tasks
+    sass: { // Begin Sass Plugin
+      dist: {
         options: {
-          map: false,
-          processors: [
-        require('autoprefixer')({
-              browsers: ['last 2 versions']
-            })
-      ]
+          sourcemap: 'none',
+          style: 'compressed'
         },
-        dist: {
-          src: 'styles/main.min.css'
-        }
+        files: [{
+          'styles/main.min.css': 'styles/scss/main.scss'
+      }]
+      }
+    },
+    postcss: { // Begin Post CSS Plugin
+      options: {
+        map: false,
+        processors: [
+      require('autoprefixer')({
+            browsers: ['last 2 versions']
+          })
+    ]
       },
-      uglify: { // Begin JS Uglify Plugin
-        build: {
-          src: ['js/*.js'],
-          dest: 'js/min/script.min.js'
-        }
-      },
-      watch: { // Compile everything into one task with Watch Plugin
-        css: {
-          files: '**/*.scss',
-          tasks: ['sass', 'postcss', 'cssmin']
-        },
-        js: {
-          files: '**/*.js',
-          tasks: ['uglify']
+      dist: {
+        src: 'styles/main.min.css'
+      }
+    },
+    uglify: { // Begin JS Uglify Plugin
+      dist: {
+        files: {
+          'js/scripts.min.js': 'js/build/**/*.js'
         }
       }
-    });
-    // Load Grunt plugins
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-postcss');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-  
-    // Register Grunt tasks
-    grunt.registerTask('default', ['watch']);
-  };
+    },
+    watch: { // Compile everything into one task with Watch Plugin
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass', 'postcss']
+      },
+      js: {
+        files: '**/*.js',
+        tasks: ['uglify']
+      }
+    }
+  });
+  // Load Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register Grunt tasks
+  grunt.registerTask('default', ['watch']);
+};
